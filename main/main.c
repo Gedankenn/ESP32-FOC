b05dd5a10097b5b49fc52e63306f8ec2de4ac4fc
Gedankenn <Gedankenn@gmail.com>
2022-10-16 21:22:39 -0300

    Calculo VF funcionando - faltando apenas implementar o pwm

--- main/main.c
+++ main/main.c
@@ -24,14 +24,13 @@ double rad;
 double theta            = 0;
 double V                = 0;
 
-const double nADC       = 4096;
+const double nADC       = 4095;
 const double max_rpm    = 10000;
-const double adc_to_rpm = max_rpm/nADC;
 const double pi         = 3.141592;
 const double rpm_to_rad = 2*pi/60;
 const double Tmax       = 12;
-const double Gf         = Tmax/(max_rpm*rpm_to_rad);
-const double gain_adc   = max_rpm/4095;
+const double Gf         = Tmax/(max_rpm);
+const double gain_adc   = max_rpm/nADC;
 const double F_adc      = 10000;
 const double T_adc      = 1/F_adc;
 
@@ -49,25 +48,27 @@ const static char *TAG_FOC = "FOC";
 
 void FOC(void *arg)
 {
-    ESP_LOGI(TAG_FOC, "Entrada da task FOC");
-    //** Leitura da referencia
-    ESP_ERROR_CHECK(adc_oneshot_read(adc1_handle, EXAMPLE_ADC1_CHAN0, &adc_raw[0][0]));
-    ESP_LOGI(TAG_ADC, "ADC%d Channel[%d] Raw Data: %d", ADC_UNIT_1 + 1, EXAMPLE_ADC1_CHAN0, adc_raw[0][0]);
-    ref = adc_raw[0][0]*adc_to_rpm;
-    ref = ref*gain_adc;
+    while(1){
+        ESP_LOGI(TAG_FOC, "Entrada da task FOC");
+        //** Leitura da referencia
+        ESP_ERROR_CHECK(adc_oneshot_read(adc1_handle, EXAMPLE_ADC1_CHAN0, &adc_raw[0][0]));
+        ESP_LOGI(TAG_ADC, "ADC%d Channel[%d] Raw Data: %d", ADC_UNIT_1 + 1, EXAMPLE_ADC1_CHAN0, adc_raw[0][0]);
+        ref = adc_raw[0][0];
+        ref = ref*gain_adc;
 
-    //* ideally the ref will be a value of angular velocity in rpm
-    rad = ref*rpm_to_rad;
-    theta = fmod((theta+rad*T_adc),(2*pi));
+        //* ideally the ref will be a value of angular velocity in rpm
+        rad = ref*rpm_to_rad;
+        theta = fmod((theta+rad*T_adc),(2*pi));
 
-    V = ref*Gf;
-    if(V >= Tmax){
-        V = Tmax;
+        V = ref*Gf;
+        if(V >= Tmax){
+            V = Tmax;
+        }
+        //atualiza_velocidade(rad);
+        printf("V = %f\n",V);
+        printf("rad = %f\n",rad);
+        vTaskDelay(xDelay*10);
     }
-    //atualiza_velocidade(rad);
-    printf("V = %f\n",V);
-    printf("rad = %f\n",rad);
-    vTaskDelay(xDelay*100);
 }
 
 void app_main(void)
@@ -78,15 +79,16 @@ void app_main(void)
     //PWM_init();
 
     printf("Minimum free heap size: %"PRIu32" bytes\n", esp_get_minimum_free_heap_size());
-    //xTaskCreate(FOC,"OPEN-LOOP",1024,NULL,5,&handle_FOC);
+    xTaskCreate(FOC,"OPEN-LOOP",2048,NULL,5,&handle_FOC);
 
-    while(1) {
-        ESP_ERROR_CHECK(adc_oneshot_read(adc1_handle, EXAMPLE_ADC1_CHAN0, &adc_raw[0][0]));
-        ESP_LOGI(TAG_ADC, "ADC%d Channel[%d] Raw Data: %d", ADC_UNIT_1 + 1, EXAMPLE_ADC1_CHAN0, adc_raw[0][0]);
-        vTaskDelay(100*xDelay);
-    }
-    ESP_ERROR_CHECK(adc_oneshot_del_unit(adc1_handle));
-    if (do_calibration1) {
-        example_adc_calibration_deinit(adc1_cali_handle);
-    }
+    // while(1) {
+    //     // ESP_ERROR_CHECK(adc_oneshot_read(adc1_handle, EXAMPLE_ADC1_CHAN0, &adc_raw[0][0]));
+    //     // ESP_LOGI(TAG_ADC, "ADC%d Channel[%d] Raw Data: %d", ADC_UNIT_1 + 1, EXAMPLE_ADC1_CHAN0, adc_raw[0][0]);
+    //     // vTaskDelay(100*xDelay);
+    //     vTaskDelay(xDelay*100);
+    // }
+    // ESP_ERROR_CHECK(adc_oneshot_del_unit(adc1_handle));
+    // if (do_calibration1) {
+    //     example_adc_calibration_deinit(adc1_cali_handle);
+    // }
 }
